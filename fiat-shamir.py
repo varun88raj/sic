cb3900a505dbbdeafff9097efe553069ebfc9a8e
varun88raj <74340962+varun88raj@users.noreply.github.com>
2022-05-16 06:53:17 +0530

    Add files via upload

--- fiat-shamir.py
+++ fiat-shamir.py
@@ -1,34 +1,11 @@
+import sympy
 import random
 
-def PrimeList():
-    i = 0
-    while(i<len(prime_list)):
-        j = i+1
-        while(j<len(prime_list)):
-            if(prime_list[j]%prime_list[i]==0):
-                prime_list.pop(j)
-                j-=1
-            j+=1
-        i+=1
+MAX_VAL = 2**16
+MIN_VAL = 2**8
 
-def generatePrime(n):
-    #p = random.randrange(2**(n-1)+1, 2**n-1)
-    while(True):
-        flag = False
-        p = random.randrange(2**(n-1)+1, 2**n-1)
-        for i in range(len(prime_list)):
-            if p%prime_list[i]==0:
-                flag = True
-                break
-        if(not flag):
-            return p
-        
-
-
-prime_list = [i+1 for i in range(1,1000)]
-PrimeList()
-p = generatePrime(10)
-q = generatePrime(10)
+p = sympy.randprime(MIN_VAL, MAX_VAL)
+q = sympy.randprime(MIN_VAL, MAX_VAL)
 n = p * q
 print("P:" , p)
 print("Q:" , q)
@@ -41,7 +18,7 @@ print("v:" , v)
 
 r = random.randrange(1,n-1)
 x = r**2 % n
-c = random.randrange(0,1)
+c = random.randint(0,1)
 y = r * (s**c) % n
 
 print("x:" , x)
