5c05b18f534c9aa9745119befe9cb7ed516b7173
varun88raj <74340962+varun88raj@users.noreply.github.com>
2022-05-16 07:33:05 +0530

    Add files via upload

--- Guillou.py
+++ Guillou.py
@@ -1,4 +1,8 @@
 import random
+import sympy
+
+MAX_VAL = 2**16
+MIN_VAL = 2**8
 
 def GCD(x, y):
   
@@ -7,36 +11,25 @@ def GCD(x, y):
   
    return x
 
+def round(n,e):        
+    r = random.randrange(2,n-2) 
+    x = r**e % n
+    print("x", x)
+    c = random.randrange(1,e)
+    y = (r*(s**c)) % n
+    print("y", y)
 
-def PrimeList():
-    i = 0
-    while(i<len(prime_list)):
-        j = i+1
-        while(j<len(prime_list)):
-            if(prime_list[j]%prime_list[i]==0):
-                prime_list.pop(j)
-                j-=1
-            j+=1
-        i+=1
+    check = (y**e) * (v**c) % n
 
-def generatePrime(n):
-    p = random.randrange(2**(n-1)+1, 2**n-1)
-    while(True):
-        flag = False
-        p = random.randrange(2**(n-1)+1, 2**n-1)
-        for i in range(len(prime_list)):
-            if p%prime_list[i]==0:
-                flag = True
-                break
-        if(not flag):
-            return p
-        
+    print("Check value:", check)
+    if check == x:
+        print("Probable")
+    else:
+        print("Improbable")
 
-prime_list = [i+1 for i in range(1,1000)]
-PrimeList()
 
-p = generatePrime(10)
-q = generatePrime(10)
+p = sympy.randprime(MIN_VAL, MAX_VAL)
+q = sympy.randprime(MIN_VAL, MAX_VAL)
 n = p * q
 phi = (p-1) * (q-1)
 
@@ -45,33 +38,22 @@ print("Q", q)
 print("n", n)
 print("phi", phi)
 
-e = random.randrange(2,n-2)
+e = 2
 while(GCD(e,phi)!=1):
-    e = random.randrange(2,n-2)
-s = random.randrange(2,n-2)
-v = None
-while(v == None):
+    e = e+1
+s = 2
+while(s<n-2):
     try:
-        s = random.randrange(2,n-2)
         t = s**e
         v = pow(t, -1, n)
     except ValueError:
         v = None
- 
-print("V", v)        
-r = random.randrange(2,n-2) 
-x = r**e % n
-print("x", x)
-c = random.randrange(1,e)
-y = (r*(s**c)) % n
-print("y", y)
+    if(not v==None):
+        break
+    s=s+1
 
-check = (y**e) * (v**c) % n
-
-print("Check value:", check)
-if check == x:
-    print("Probable")
-else:
-    print("Improbable")
-
+print("V", v)
+for i in range(0,14):
+    print("Round",i+1)
+    round(n,e)
 
